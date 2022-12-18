4ee91fd47a37325da83bdb6311badf439574e0e9
unknown <vasilinad1212@gmail.com>
2022-12-18 13:29:47 +0300

    week1 13

---
+++ index.js
@@ -0,0 +1,26 @@
+let YourName = prompt('Как тебя зовут?', '');
+alert(`Привет, ${YourName}!`);
+
+function summ(x, y) {
+    let firstNum = prompt('Введите первое значение', '');
+    let secondNum = prompt('Введите второе значение', '');
+    alert( `${firstNum} плюс ${secondNum} равно ${+firstNum + +secondNum}` )
+}
+
+function subtract(x, y) {
+    let firstNum = prompt('Введите первое значение?', '');
+    let secondNum = prompt('Введите второе значение', '');
+    alert( `${firstNum} минус ${secondNum} равно ${+firstNum - +secondNum}` )
+}
+
+function mult(x, y) {
+    let firstNum = prompt('Введите первое значение?', '');
+    let secondNumber = prompt('Введите второе значение', '');
+    alert( `${firstNum} умножить на ${secondNum} равно ${+firstNum * +secondNum}` )
+}
+
+function division(x, y) {
+    let firstNum = prompt('Введите первое значение?', '');
+    let secondNum = prompt('Введите второе значение', '');
+    alert( `${firstNum} разделить на ${secondNum} равно ${+firstNum / +secondNum}` )
+}
